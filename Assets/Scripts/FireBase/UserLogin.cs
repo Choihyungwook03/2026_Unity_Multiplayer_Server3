95e2eb6a2bd1e5ebaca901778b497870e488486b
Choihyungwook03 <ejyong1212@g.shingu.ac.kr>
2026-05-28 17:45:10 +0900

    [Feat] FireBase 재설정

---
+++ Assets/Scripts/FireBase/UserLogin.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using Firebase.Database;
+using UnityEngine.UI;
+using PimDeWitte.UnityMainThreadDispatcher;
+
+public class UserLogin : MonoBehaviour
+{
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    UnityMainThreadDispatcher dispatcher;
+
+    [SerializeField] InputField NickNameInput;
+    [SerializeField] Text checkText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        database = FirebaseDatabase.GetInstance(
+            "https://shingutest-94ea9-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        );
+
+        reference = database.RootReference;
+        dispatcher = UnityMainThreadDispatcher.Instance();
+    }
+
+    public void OnClickLogin()
+    {
+        string nickname = NickNameInput.text.Trim();
+        if (string.IsNullOrEmpty(nickname))
+        {
+            checkText.text = "닉네임을 입력해주세요.";
+            return;
+        }
+        reference.Child("UserInfo").OrderByChild("NickName").EqualTo(nickname).GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                dispatcher.Enqueue(() =>
+                {
+                    checkText.text = "Firebase 읽기 오류";
+                });
+                return;
+            }
+
+            DataSnapshot snapshot = task.Result;
+
+            if (!snapshot.HasChildren)
+            {
+                dispatcher.Enqueue(() =>
+                {
+                    checkText.text = "존재하지 않는 닉네임입니다.";
+                });
+                return;
+            }
+
+            foreach (DataSnapshot userSnapShot in snapshot.Children)
+            {
+                string userKey = userSnapShot.Key;
+
+                dispatcher.Enqueue(() =>
+                {
+                    PlayerPrefs.SetString("UserKey", userKey);
+                    PlayerPrefs.SetString("UserNickname", nickname);
+                    PlayerPrefs.Save();
+
+                    checkText.text = "로그인 성공";
+                });
+                break;
+            }
+        });
+    }
+}
